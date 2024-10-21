230917086e5cf066ce6442e119c1e1ab86c2fe7f
Noclist <noclistmyhoa2019@gmail.com>
2024-10-21 09:16:46 +0700

    update VS

--- QuanLyCuaHangBanDienThoai/QuanLyCuaHangBanDienThoai/Program.cs
+++ QuanLyCuaHangBanDienThoai/QuanLyCuaHangBanDienThoai/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyCuaHangBanDienThoai
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form_DienThoai());
+            Application.Run(new Form_DangNhap());
         }
     }
 }
